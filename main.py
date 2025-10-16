4dfc9c03a64f702a0c686f13b8b5a7a1ebf8d703
saurabh007007 <loverboybhol143@gmail.com>
2025-10-16 23:48:36 +0530

    feat:Added the complete file opertaion

--- main.py
+++ main.py
@@ -279,7 +279,7 @@ def open_file(event=None):
         return 
     except:
         return
-    main_application.title(os.path.basename(url))
+    # main_application.title(os.path.basename(url))
     
     
 file.add_command(label='Open',image=open_icon,compound=tk.LEFT,accelerator='Ctrl+O',command=open_file)
@@ -304,8 +304,46 @@ def save_file(event=None):
     
     
 file.add_command(label='Save',image=save_icon,compound=tk.LEFT,accelerator='Ctrl+S',command=save_file)
-file.add_command(label='Save As',image=save_icon,compound=tk.LEFT,accelerator='Ctrl+Alt+S')
-file.add_command(label='Exit',image=exit_icon,compound=tk.LEFT,accelerator='Ctrl+Q')
+
+# save as file functionality 
+def save_as_file(event=None):
+    global url 
+    try:
+        content=text_editor.get(1.0,tk.END)
+        url=filedialog.asksaveasfile(mode='w',defaultextension='.txt',filetypes=(('Text File','*.txt'),('All files','*.*')))
+        url.write(content)
+        url.close()
+        status_bar.config(text='File Saved')
+    except:
+        return 
+    # main_application.title(os.path.basename(url))
+file.add_command(label='Save As',image=save_icon,compound=tk.LEFT,accelerator='Ctrl+Alt+S',command=save_as_file)
+#exit functionality 
+def exit_func(event=None):
+    global url,text_change
+    try:
+        if text_change:
+            mbox=messagebox.askyesnocancel('Warning','Do you want to save the file ?')
+            if mbox is True:
+                if url:
+                    content=text_editor.get(1.0,tk.END)
+                    with open(url,'w',encoding='utf-8') as fw:
+                        fw.write(content)
+                    main_application.destroy()
+                else:
+                    content2=text_editor.get(1.0,tk.END)
+                    url=filedialog.asksaveasfile(mode='w',defaultextension='.txt',filetypes=(('Text File','*.txt'),('All files','*.*')))
+                    url.write(content2)
+                    url.close()
+                    main_application.destroy()
+            elif mbox is False:
+                main_application.destroy()
+        else:
+            main_application.destroy()
+    except:
+        return
+
+file.add_command(label='Exit',image=exit_icon,compound=tk.LEFT,accelerator='Ctrl+Q',command=exit_func)
 
 # edit commands 
 edit.add_command(label='Copy',image=copy_icon,compound=tk.LEFT,accelerator='Ctrl+C')
